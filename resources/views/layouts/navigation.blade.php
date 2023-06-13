8c078d3c12683b4592628a72a4fad304a15644b4
harizjamaluddin <hariz@myiexplorer.com>
2023-06-13 23:24:08 +0800

    cleaning up

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -17,7 +17,7 @@
                     </x-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('tasks')" :active="request()->routeIs('tasks')">
+                    <x-nav-link :href="route('tasks')" :active="request()->routeIs('tasks', 'edit')">
                         {{ __('Tasks') }}
                     </x-nav-link>
                 </div>
--- routes/web.php
+++ routes/web.php
@@ -30,16 +30,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('tasks/{id}', [TaskController::class, 'destroy'])->name('destroy');
 });
 
-/*Route::get('todos/{todoId}', 'TaskController@show');
-Route::get('new-todo', 'TaskController@create');
-Route::post('store-todo', 'TaskController@store');
-Route::get('todos/{todoId}/edit','TaskController@edit');
-Route::post('todos/{todoId}/update','TaskController@update');
-Route::get('todos/{todoId}/delete','TaskController@destroy');
-Route::get('todos/{todoId}/complete','TaskController@complete');
-
-Route::get('completed',function(){
-    return view('completed')->with('todos',Todo::all());
-});*/
-
 require __DIR__.'/auth.php';
