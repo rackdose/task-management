83bc0a6168e14306b275a74dfd4c21f662aa140c
harizjamaluddin <hariz@myiexplorer.com>
2023-06-13 23:10:56 +0800

    added timeout for messages

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -15,12 +15,14 @@
 
                     <!-- Validation Errors -->
                     <x-auth-validation-errors class="mb-4" :errors="$errors" />
-                    @if(Session::has('success'))
-                        <div class="mt-4 flex items-center bg-blue-500 text-white text-sm font-bold px-4 py-3" role="alert">
+                    <div x-data="{ showMessage: true }" x-show="showMessage" x-init="setTimeout(() => showMessage = false, 3000)">
+                        @if (session()->has('success'))
+                        <div class="mt-4 flex items-center bg-blue-500 text-white text-sm font-bold px-4 py-3" id="msg" role="alert">
                             <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.432 0c1.34 0 2.01.912 2.01 1.957 0 1.305-1.164 2.512-2.679 2.512-1.269 0-2.009-.75-1.974-1.99C9.789 1.436 10.67 0 12.432 0zM8.309 20c-1.058 0-1.833-.652-1.093-3.524l1.214-5.092c.211-.814.246-1.141 0-1.141-.317 0-1.689.562-2.502 1.117l-.528-.88c2.572-2.186 5.531-3.467 6.801-3.467 1.057 0 1.233 1.273.705 3.23l-1.391 5.352c-.246.945-.141 1.271.106 1.271.317 0 1.357-.392 2.379-1.207l.6.814C12.098 19.02 9.365 20 8.309 20z"/></svg>
-                            <p>{{Session::get('success')}}</p>
+                            <p>{{ session()->get('success') }}</p>
                         </div>
-                    @endif
+                        @endif
+                    </div>
 
                     <form class="flex flex-col space-y-4" method="POST" action="{{ route('store') }}">
                         @csrf
@@ -48,14 +50,7 @@
                                 </div>
                                 <input datepicker datepicker-format="dd-mm-yyyy" type="text" :value="old('due_date')" class="bg-gray-50 py-3 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" placeholder="Due Date" name="due_date" required>
                             </div>
-
-                            <!--<select name="status" class="w-1/3 py-3 px-4 bg-gray-100 rounded-lg">
-                                <option value="" selected>Status</option>
-                                <option value="1">Completed</option>
-                                <option value="2">Not Completed</option>
-                            </select>-->
                         </div>
-
                         <button class="w-28 py-4 px-8 text-white bg-green-500 rounded-lg">Add</button>
                     </form>
                 </div>
@@ -63,6 +58,8 @@
                 <div class="lg:px-8 mx-auto py-8 py-6 bg-white">
                     @foreach ($tasks as $task)
                         <div class="py-4 flex item-center px-3 border-b border-gray-300 rounded-lg">
+
+                            <!-- task list -->
                             <div class="flex-1 pr-2">
                                 <h3 class="text-lg font-semibold">{{ $task->title }}</h3>
                                 <h3 class="text-sm font-semibold">{{ $task->user->name }}</h3>
